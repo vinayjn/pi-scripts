0a60cd98b7c9d4cf724a10649d0ca65fcc5994e2
Vinay Jain <vinay.jn7@gmail.com>
2023-11-22 09:11:37 +0100

    Fix home directory path

--- setup.sh
+++ setup.sh
@@ -63,8 +63,8 @@ git clone https://github.com/pia-foss/manual-connections
 
 echo "Configuring ZSH"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
 
 echo "Enter PIA Username:"
 read -r pia_username
